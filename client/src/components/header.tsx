953186121ccc6eb2fd88629f7d4407960f14d2e8
faizan-seamlessideas <faizan@seamlessideas.co.uk>
2024-04-23 13:55:01 +0500

    completed responsive header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom"
 import { Button } from "./ui/button"
-
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { AlignJustify } from "lucide-react"
 const Header = () => {
   return (
     <div className="bg-zinc-500 h-14 " >
@@ -10,7 +16,7 @@ const Header = () => {
             {/* <img src="/logo.svg" alt="logo" className="h-8" /> */}
             <span className="text-white text-lg ml-2">Doctor Management</span>
           </div>
-          <div className="flex items-center gap-5 pb-2">
+          <div className="hidden sm:flex  items-center gap-5 pb-2">
             <Link to="/" className="nav-link">Home</Link>
             <Link to="/" className="nav-link">About</Link>
             <Link to="/" className="nav-link">Contact Us</Link>
@@ -18,7 +24,12 @@ const Header = () => {
 
               <Button size={"sm"} variant={"outline"}  >Login</Button>
               <Button size={"sm"}  >Apply As Doctor</Button>
+
             </div>
+          </div>
+          <div className="inline sm:hidden mr-2">
+
+            <MobileHeader />
           </div>
         </div>
       </div>
@@ -27,3 +38,25 @@ const Header = () => {
 }
 
 export default Header
+
+const MobileHeader = () => {
+  return (
+    <Sheet>
+      <SheetTrigger><AlignJustify color="white" /></SheetTrigger>
+      <SheetContent side={"right"} className="bg-zinc-500">
+
+        <div className="flex items-center justify-center pb-2 mt-5">
+          {/* <img src="/logo.svg" alt="logo" className="h-8" /> */}
+          <span className="text-white text-lg ml-2">Doctor Management</span>
+        </div>
+        <div className="flex flex-col items-center gap-5 pb-2">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/" className="nav-link">About</Link>
+          <Link to="/" className="nav-link">Contact Us</Link>
+          <Button size={"sm"} variant={"outline"}  >Login</Button>
+          <Button size={"sm"}  >Apply As Doctor</Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
